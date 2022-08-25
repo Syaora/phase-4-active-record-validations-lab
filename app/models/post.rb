26f0282fe079b64ffb542136c4c27a2740fea585
Elizabeth Nguyen <saper209@yahoo.com>
2022-08-25 01:40:52 -0700

    Practice

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Non-Fiction', 'Fiction']}
+  validate :clickybaity?
+
+  TITLES = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickybaity?
+    if TITLES.none? { |name| name.match title }
+      errors.add(:title, "Title must be a clickbait")
+    end
+  end
 end
